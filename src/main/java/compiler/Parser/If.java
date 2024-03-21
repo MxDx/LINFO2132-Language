943ecaf698fcf4cdf55d3347a84efa30d0c741d5
MxDx <maxime.dxl2002@gmail.com>
2024-03-21 18:48:39 +0100

    if else and struct grammar

--- src/main/java/compiler/Parser/If.java
+++ src/main/java/compiler/Parser/If.java
@@ -1,8 +1,17 @@
 package compiler.Parser;
 
+import compiler.Lexer.Keyword;
+import compiler.Lexer.Symbol;
+
 public class If extends Node{
+
+    final static Symbol ELSE = new Keyword("else");
     Expression expression;
     Block block;
+
+    Node elseStatement;
+
+
     public If(Parser parser) throws Exception {
         super(parser);
         parser.getNext();
@@ -14,14 +23,45 @@ public class If extends Node{
         parser.match(Parser.OPEN_BRACES);
         block = new Block(parser).parse();
         parser.match(Parser.CLOSE_BRACES);
+
+        if (parser.currentToken.equals(ELSE)){
+            elseStatement = new ELse(parser).parse();
+        }
+
         return this;
     }
 
     @Override
     public String toString() {
-        return "\"IF_Statement\": {\n"
+        String str =  "\"IF_Statement\": {\n"
                 + "\"expression\": \""+ expression.toString() + "\",\n"
-                + "\"block\": " + block.toString() + "\n"
-                + '}';
+                + "\"block\": " + block.toString() + "\n";
+        if (elseStatement != null){
+            str += ",\n" + elseStatement.toString();
+        }
+        str += '}';
+        return str;
+    }
+
+    private static class ELse extends Node {
+        Block block;
+
+        public ELse(Parser parser) throws Exception {
+            super(parser);
+            parser.getNext();
+        }
+        public Node parse() throws Exception {
+            parser.match(Parser.OPEN_BRACES);
+            block = new Block(parser).parse();
+            parser.match(Parser.CLOSE_BRACES);
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return "\"ELSE_Statement\": {\n"
+                    + "\"block\": " + block.toString() + "\n"
+                    + '}';
+        }
     }
 }
