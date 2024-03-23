d16ee08c2da2e0bc72b30622184556130601ad78
MxDx <maxime.dxl2002@gmail.com>
2024-03-23 19:32:38 +0100

    Adding first few tests

---
+++ test/TestParser.java
@@ -0,0 +1,83 @@
+import compiler.Lexer.Lexer;
+import compiler.Lexer.VarType;
+import compiler.Parser.*;
+import org.junit.Test;
+
+import java.io.StringReader;
+
+import static org.junit.Assert.*;
+
+public class TestParser {
+    private static Starting parse(String input) throws Exception {
+        StringReader reader = new StringReader(input);
+        Lexer lexer = new Lexer(reader);
+        Parser parser = new Parser(lexer);
+        return parser.getAST();
+    }
+    @Test
+    public void testBasicDeclaration() throws Exception {
+        String input = "int a;";
+        Starting root = parse(input);
+
+        Statements statement = (Statements) root;
+        Statements.Statement stmt = (Statements.Statement) statement.statements.get(0);
+        Declaration decl = (Declaration) stmt.content;
+        assertEquals(new VarType("int"), decl.type);
+        assertEquals("a", decl.identifier);
+
+        assertNull(decl.assignment);
+
+        input = "int a = 10;";
+        root = parse(input);
+
+        statement = (Statements) root;
+        stmt = (Statements.Statement) statement.statements.get(0);
+        decl = (Declaration) stmt.content;
+        assertEquals(new VarType("int"), decl.type);
+        assertEquals("a", decl.identifier);
+        assertNotNull(decl.assignment);
+
+        Expression expression = (Expression) decl.assignment;
+        Expression.Value value = (Expression.Value) expression.corps;
+        assertEquals("10", value.value.getValue());
+    }
+
+    @Test
+    public void testComplexeDeclaration() throws Exception {
+        String input = "int[] a;";
+        Starting root = parse(input);
+
+        Statements statement = (Statements) root;
+        Statements.Statement stmt = (Statements.Statement) statement.statements.get(0);
+        Declaration decl = (Declaration) stmt.content;
+
+        VarType expectedType = new VarType("int");
+        expectedType.setVector();
+        assertEquals(expectedType, decl.type);
+
+        assertEquals("a", decl.identifier);
+        assertNull(decl.assignment);
+
+        input = "int[] a = 10 * 10;";
+        root = parse(input);
+
+        statement = (Statements) root;
+        stmt = (Statements.Statement) statement.statements.get(0);
+        decl = (Declaration) stmt.content;
+        expectedType = new VarType("int");
+        expectedType.setVector();
+        assertEquals(expectedType, decl.type);
+        assertEquals("a", decl.identifier);
+        assertNotNull(decl.assignment);
+
+        Expression expression = (Expression) decl.assignment;
+        Expression.Operation operation = (Expression.Operation) expression;
+        Expression.Value value = (Expression.Value) operation.left;
+        assertEquals("10", value.value.getValue());
+        value = (Expression.Value) operation.right;
+        assertEquals("10", value.value.getValue());
+        assertEquals("*", operation.operation);
+        assertEquals("10", value.value.getValue());
+    }
+
+}
