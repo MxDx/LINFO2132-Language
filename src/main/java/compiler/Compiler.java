804aa45dab4c0843cea4e2c2b99e1e9c109ff35c
GigaUltimatumXVI <adriennotte@hotmail.com>
2024-03-25 15:37:44 +0100

    good but not very good

--- src/main/java/compiler/Compiler.java
+++ src/main/java/compiler/Compiler.java
@@ -23,12 +23,12 @@ public class Compiler {
         if ((showLexer ||showParser) && args.length < 2) {
             System.out.println("No input file");
             return;
-        }
-        */
+        }*/
+
         //String inputPath = args[(showLexer||showParser ? 1 : 0)];
-        boolean showParser = true;
-        boolean showLexer = false;
         String inputPath = "src/main/java/compiler/test.txt";
+        boolean showLexer = false;
+        boolean showParser = true;
         System.out.println("inputPath: " + inputPath); //LOCAL: String inputPath = "src/main/java/compiler/test.txt";
         Lexer lex = lexerGetter(inputPath, showLexer);
         parserGetter(lex, showParser);
